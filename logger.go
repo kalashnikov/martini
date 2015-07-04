789006221ec89bed536c84d426be46d0e97ffad7
kalakuo <kalaexj@gmail.com>
2015-07-05 02:01:47 +0800

    Add color and merge into one line per request

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package martini
 
 import (
+	"github.com/fatih/color"
 	"log"
 	"net/http"
 	"time"
@@ -9,6 +10,7 @@ import (
 // Logger returns a middleware handler that logs the request as it goes in and the response as it goes out.
 func Logger() Handler {
 	return func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
+		const layout = "Jan 2, 2006 at 3:04pm (MST)"
 		start := time.Now()
 
 		addr := req.Header.Get("X-Real-IP")
@@ -19,11 +21,16 @@ func Logger() Handler {
 			}
 		}
 
-		log.Printf("Started %s %s for %s when %v", req.Method, req.URL.Path, addr, start)
-
 		rw := res.(ResponseWriter)
 		c.Next()
 
-		log.Printf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start))
+		red := color.New(color.FgRed).SprintFunc()
+		blue := color.New(color.FgBlue).SprintFunc()
+		cyan := color.New(color.FgCyan).SprintFunc()
+		green := color.New(color.FgGreen).SprintFunc()
+		yellow := color.New(color.FgYellow).SprintFunc()
+		magenta := color.New(color.FgMagenta).SprintFunc()
+		log.Printf("[%v, %v] %v | %s %s from %s\n", blue(start.Format(layout)), cyan(time.Since(start)),
+			red(rw.Status()), yellow(req.Method), green(req.URL.RequestURI()), magenta(addr))
 	}
 }
